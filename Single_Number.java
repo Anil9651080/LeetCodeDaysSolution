ac8d468435847b3c24d3739903e0bd73acc72ccd
Anil Sharma <anilsharma9651080@gmail.com>
2023-03-20 23:21:11 +0530

    First DaysCode of LeetCode

---
+++ Single_Number.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+// import javax.swing.text.html.HTMLDocument.Iterator;
+
+class Single_Number {
+    public int singleNumber(int[] nums) {
+        // int result = 0;
+        // for (int number: nums) {
+        // result ^= number; // Logical XOR operator
+        // }
+        // return result;
+        HashSet<Integer> set = new HashSet<Integer>();
+        for (int n : nums) {
+            if (!set.add(n))
+                set.remove(n);
+        }
+        Iterator<Integer> it = set.iterator();
+        return it.next();
+    }
+}
